c189b4b57f33e448e2bed7edd0e0689966949022
dustryder <dust-ryder@hotmail.com>
2020-05-06 17:33:08 +1200

    New Stuff

--- app.py
+++ app.py
@@ -9,10 +9,51 @@ from flask import Flask, render_template, request, session
 import mysql.connector
 from mysql.connector import Error, errorcode
 from config_db import config
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
 
 from flask import Flask
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'G04ps8f_7Wm3kRyDc480Dg8884'
+admin = Admin(app, base_template='layout.html', template_mode='bootstrap3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Gryphon11@localhost/devices'
+db = SQLAlchemy(app)
+
+class Device(db.Model):
+    __tablename__ = 'device'
+
+    device_id = db.Column(db.INTEGER, primary_key=True)
+    device_name = db.Column(db.String(30))
+    device_type = db.Column(db.String(30))
+    os_type = db.Column(db.String(10))
+    os_version = db.Column(db.String(10))
+    ram = db.Column(db.String(10))
+    device_cpu = db.Column(db.String(50))
+    device_bit = db.Column(db.CHAR(2))
+    resolution = db.Column(db.String(30))
+    grade = db.Column(db.String(10))
+    uuid = db.Column(db.String(50))
+    acquisition_date = db.Column(db.Date)
+
+
+class Employee(db.Model):
+    __tablename__ = 'employee'
+
+    employee_id = db.Column(db.INTEGER, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    location = db.Column(db.String(30))
+    permissions = db.Column(db.INTEGER)
+
+class UserView(ModelView):
+
+    create_modal = True
+    edit_modal = True
+
+admin.add_view(UserView(Device, db.session))
+admin.add_view(UserView(Employee, db.session))
+
 #secret key needed for securely signing the session cookie.
 
 #Function for get Database connection
@@ -41,12 +82,12 @@ def make_session_permanent():
 @app.route("/", methods = ['POST', 'GET'])
 def home():
 
-    permission = 0
+    permission = 10
 
     try:
         connection = get_db_connection()
         mycursor = connection.cursor()
-        mycursor.execute("SELECT device_id, device_name, status, device_type, os_type, os_version, grade FROM Device")
+        mycursor.execute("SELECT device_id, device_name, first_name, device_type, os_type, os_version, grade FROM devicestatus ORDER BY device_id")
         device_table = mycursor.fetchall()
 
         mycursor.execute("SELECT employee_id, first_name FROM employee")
@@ -64,13 +105,15 @@ def home():
         employee_id, employee_name = employee_id.split(',')
 
         if available != "available":
-            mycursor.execute(f"UPDATE device SET status = 'available' WHERE device_id = {device_id}")
+            query = f"DELETE FROM deviceloan WHERE device_id = {device_id}"
+            mycursor.execute(query)
         else:
-            mycursor.execute(f"UPDATE device SET status = '{employee_name}' WHERE device_id = {device_id}")
+            query = f"INSERT INTO deviceloan (device_id, employee_id) VALUE ({device_id},{employee_id})"
+            mycursor.execute(query)
         connection.commit()
 
         #We are retrieving the devices/employees again to update the homepage with the changes we've made
-        mycursor.execute("SELECT device_id, device_name, status, device_type, os_type, os_version, grade FROM Device")
+        mycursor.execute("SELECT device_id, device_name, first_name, device_type, os_type, os_version, grade FROM devicestatus ORDER BY device_id")
         device_table = mycursor.fetchall()
 
         mycursor.execute("SELECT employee_id, first_name FROM employee")
